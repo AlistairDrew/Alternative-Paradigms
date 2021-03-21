f02f52ed13c40989797b3573ab9adc78147d8d86
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-03-21 14:09:46 +0000

    BF template

--- Set Excercise/bf.py
+++ Set Excercise/bf.py
@@ -5,17 +5,17 @@ def evaluate(code):
  stack = []
  while codeptr < len(code):
 
- command = code[codeptr]
+    command = code[codeptr]
 
  if command == ">":
- cellptr += 1
+    cellptr += 1
  if cellptr == len(cells): cells.append(0) # if memory too small, append a 0
  if command == "<":
- cellptr = 0 if cellptr <= 0 else cellptr - 1
+    cellptr = 0 if cellptr <= 0 else cellptr - 1
  if command == "+":
- cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
+    cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
  if command == "-":
- cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
+    cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
 
  # messing with Unicode to get the ' and . operators working
 # if command == ".": sys.stdout.write(chr(cells[cellptr]))
@@ -25,24 +25,24 @@ def evaluate(code):
  if command == ",": cells[cellptr] = ord(input())
 
  if command == "[" and cells[cellptr] == 0: # skip or end the loop
- loop = 1
+    loop = 1
  while loop > 0: # there could be nested loops
- codeptr += 1 # which need to be skipped
- c = code[codeptr]
+    codeptr += 1 # which need to be skipped
+    c = code[codeptr]
  if c == '[' :
- loop += 1
+    loop += 1
  elif c == ']' :
- loop -= 1 # if loop gets 0 we found the closing ]
+    loop -= 1 # if loop gets 0 we found the closing ]
  if command == "]": # and cells[cellptr] != 0: # end of loop but need to do more iterations
  # definitions of bf ] vary a bit
- loop = 1
+    loop = 1
  while loop > 0: # there could be nested loops
- codeptr -= 1 # which need to be skipped
+    codeptr -= 1 # which need to be skipped
  c = code[codeptr]
  if c == '[' :
- loop -= 1
+    loop -= 1
  elif c == ']' :
- loop += 1 # if loop gets 0 we found the closing [
+    loop += 1 # if loop gets 0 we found the closing [
  codeptr -= 1 # one left becaus the main increments (as below)
  codeptr+=1
  return cells
